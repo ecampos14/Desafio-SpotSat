7d7c4f4b17ee70cc84b599d29f52a1f2f38113ae
ecampos14 <88864112+ecampos14@users.noreply.github.com>
2023-05-19 19:01:38 -0300

    Adicionando Springboot

---
+++ backend-desafio/backend-desafio/src/main/java/spotSat/backenddesafio/BackendDesafioApplication.java
@@ -0,0 +1,13 @@
+package spotSat.backenddesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafioApplication.class, args);
+	}
+
+}
